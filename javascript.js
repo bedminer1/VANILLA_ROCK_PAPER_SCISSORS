41a4993dfbfa7e18a3f7ef5d7fe7bf44ab3dfa00
bedminer1 <alexander.seah.min.rui@gmail.com>
2023-10-16 11:32:25 +0800

    drafted random computer selection

---
+++ javascript.js
@@ -0,0 +1,45 @@
+let playerScore = 0;
+let computerScore =0
+let roundWinner = ''
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        roundWinner = 'tie'
+    }
+
+    if (
+        (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') ||
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK')
+      ) {
+        playerScore++
+        roundWinner = 'player'
+      }
+      if (
+        (computerSelection === 'ROCK' && playerSelection === 'SCISSORS') ||
+        (computerSelection === 'SCISSORS' && playerSelection === 'PAPER') ||
+        (computerSelection === 'PAPER' && playerSelection === 'ROCK')
+      ) {
+        computerScore++
+        roundWinner = 'computer'
+      }
+}
+
+function getRandomChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 0:
+            return 'ROCK'
+        case 1:
+            return 'PAPER'
+        case 2:
+            return 'SCISSORS'
+    }
+}
+
+computerSelection = getRandomChoice()
+playerSelection = 'ROCK'
+playRound(playerSelection,computerSelection)
+
+console.log(computerSelection)
+console.log(roundWinner)
